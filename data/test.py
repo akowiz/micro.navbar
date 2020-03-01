bcadd8ac5c7ef83257ce364861f0c85e4de42be9
Sylvain Pouilly <akorune@gmail.com>
2020-03-02 06:25:10 +0800

    Added a python file to be used in our unit tests.

---
+++ data/test.py
@@ -0,0 +1,27 @@
+DEF_TITLE = 'title'
+DM_NONE = 0
+
+def combine_data(x, y):
+    return x+y
+
+def display_something():
+    return ''
+
+thisismoot = 3
+
+class Foo():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return ''
+
+    def func_dummy(self, a, b):
+        pass
+
+class Bar(Toto):
+    def func_another(self, c):
+        pass
+
+    def __repr__(self):
+        return 'Bar()'
