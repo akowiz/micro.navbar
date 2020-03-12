ec87a451081703fe4d90b75dbb27b7df8e147639
Sylvain Pouilly <akorune@gmail.com>
2020-03-12 14:11:10 +0800

    Cleanup the unit tests, still doing nothing.

--- test/u_test.lua
+++ test/u_test.lua
@@ -8,25 +8,21 @@ local lu = require('luaunit')
 local nb = require('navbar_python')
 
 
-
 TestBuffer = {} --class
 
-    function TestBuffer:setUp()
-        -- set up tests
-        self.a = 1
-        self.s = 'hop'
-        self.t1 = {1,2,3}
-        self.t2 = {one=1,two=2,three=3}
-        self.t3 = {1,2,three=3}
-    end
+function TestBuffer:setUp()
+    -- set up tests
+    self.a = 1
+end
 
-    function TestBuffer:test1_read()
-        assert(1 == 1)
-    end
+function TestBuffer:test1_read()
 
-    function TestBuffer:test2_read()
-        lu.assertEquals(1, 1)
-    end
+    assert(self.a == 1)
+end
+
+function TestBuffer:test2_read()
+    lu.assertEquals(self.a, 1)
+end
 
 -- class TestBuffer
 
