713ab4e6b9bb76e7927f479bd21d0587d8cd185d
Sylvain Pouilly <akorune@gmail.com>
2020-03-12 16:38:11 +0800

    Added a unit test for the split function. All tests passes.

--- test/u_test.lua
+++ test/u_test.lua
@@ -8,23 +8,30 @@ local lu = require('luaunit')
 local nb = require('navbar_python')
 
 
-TestBuffer = {} --class
+TestSplit = {}
 
-function TestBuffer:setUp()
-    -- set up tests
-    self.a = 1
+function TestSplit:setUp()
+    -- Set up tests
+    self.tList = {
+        { 'x!yy!zzz!@', '#', nil, {'x!yy!zzz!@'} },
+        { 'x!yy!zzz!@', '!', nil, {'x', 'yy', 'zzz', '@'} },
+        { 'x!yy!zzz!@', '!', 1,   {'x', 'yy!zzz!@'} },
+        { 'x!yy!zzz!@', '!', 2,   {'x', 'yy', 'zzz!@'} },
+        { 'x!yy!zzz!@', '!', 3,   {'x', 'yy', 'zzz', '@'} },
+    }
 end
 
-function TestBuffer:test1_read()
-
-    assert(self.a == 1)
+function TestSplit:test_split_properly()
+    for k, test in pairs(self.tList) do
+        input = test[1]
+        sep = test[2]
+        nmax = test[3]
+        expected = test[4]
+        result = input:split(sep, nmax)
+        lu.assertEquals(expected, result)
+    end
 end
-
-function TestBuffer:test2_read()
-    lu.assertEquals(self.a, 1)
-end
-
--- class TestBuffer
+-- class TestSplit
 
 
 
