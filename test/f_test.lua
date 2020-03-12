e715dc7f52d46c15a81754a9fb3d72089d09d4d5
Sylvain Pouilly <akorune@gmail.com>
2020-03-12 13:07:42 +0800

    Added a dummy functional test.

---
+++ test/f_test.lua
@@ -0,0 +1,40 @@
+#!/bin/env lua
+
+--------------------------------------------------------------------------------
+-- Main Functional Test Script
+--------------------------------------------------------------------------------
+
+local lu = require('luaunit')
+local nb = require('navbar')
+
+
+
+TestBuffer = {} --class
+
+    function TestBuffer:setUp()
+        -- set up tests
+        self.a = 1
+        self.s = 'hop'
+        self.t1 = {1,2,3}
+        self.t2 = {one=1,two=2,three=3}
+        self.t3 = {1,2,three=3}
+    end
+
+    function TestBuffer:test1_read()
+        assert(1 == 1)
+    end
+
+    function TestBuffer:test2_read()
+        lu.assertEquals(1, 1)
+    end
+
+-- class TestBuffer
+
+
+--------------------------------------------------------------------------------
+-- Running the test
+--------------------------------------------------------------------------------
+
+cal runner = lu.LuaUnit.new()
+-- runner:setOutputType("junit", "junit_xml_file")
+os.exit( runner:runSuite() )
