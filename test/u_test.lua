624266cc96b25fe78b96e772285e7fb7031d8c24
Sylvain Pouilly <akorune@gmail.com>
2020-03-14 12:48:38 +0800

    Added unit test for the Node object (counting children, appending a child). Test fails as expected.

--- test/u_test.lua
+++ test/u_test.lua
@@ -8,7 +8,7 @@ local lu = require('luaunit')
 local nbp = require('navbar_python')
 
 
-TestSplit = {}
+TestSplit = {} -- class
 
 function TestSplit:setUp()
     -- Set up tests
@@ -33,6 +33,62 @@ function TestSplit:test_split_properly()
 end
 -- class TestSplit
 
+
+TestNode = {} --  class
+
+function TestNode:setUp()
+    -- Set up our tests
+    local no_children = nbp.Node:new("No Children")
+    local with_children = nbp.Node:new("With Children")
+    local child1 = nbp.Node:new("Children 1 with Children")
+    local child2 = nbp.Node:new("Children 2")
+    local child3 = nbp.Node:new("Children 3")
+    local childA = nbp.Node:new("Children A")
+    local childB = nbp.Node:new("Children B")
+
+    table.insert(with_children.children, child1)
+    table.insert(with_children.children, child2)
+    table.insert(with_children.children, child3)
+    table.insert(child1.children, childA)
+    table.insert(child1.children, childB)
+
+    self.nList = {
+        no_children,
+        with_children,
+        child1,
+        child2,
+        child3,
+        childA,
+        childB,
+    }
+end
+
+function TestNode:test_append_child()
+    local parent = nbp.Node:new("Parent")
+    local children = parent.children
+    lu.assertEquals(children, {})
+
+    for i=1,5 do
+        local node = nbp.Node:new("Node"..i)
+        parent:append(node)
+        lu.assertEquals(#children, i)
+        lu.assertEquals(parent, node.parent)
+        lu.assertEquals(children[#children], node)
+    end
+end
+
+function TestNode:test_count_children()
+    local no_children = self.nList[1].children
+    lu.assertEquals(#no_children, 0)
+
+    local with_children = self.nList[2].children
+    lu.assertEquals(#with_children, 3)
+
+    local child1 = self.nList[3].children
+    lu.assertEquals(#child1, 2)
+end
+
+-- class TestNode
 
 
 
