0e461fd59a757063a395f00cfb1fbe463c246ed9
Sylvain Pouilly <akorune@gmail.com>
2020-03-21 18:40:58 +0800

    Expanded unit test for lua buffer. Test fails.

--- test/u_test.lua
+++ test/u_test.lua
@@ -73,6 +73,9 @@ function TestMatchLuaItem:setUp()
         { 'local function obj.F4()',lgl.Node('obj.F4', lgl.T_FUNCTION) },
         { 'function F5',            nil }, -- this is valid lua but we will ignore
         { 'functionF6()',           nil },
+        { 'function O1.O2:F7()',        lgl.Node('O1.O2:F7', lgl.T_FUNCTION) },
+        { 'local function O1.O2:F8()',  lgl.Node('O1.O2:F8', lgl.T_FUNCTION) },
+        { 'function tree.Node:do_some()',  lgl.Node('tree.Node:do_some', lgl.T_FUNCTION) },
     }
 end
 
@@ -243,74 +246,119 @@ function TestLuaBuffer:setUp()
     self.bList = {}
 
     self.bList['functions_only'] = 'function F3(c)\n\nfunction F4(d)\n\nfunction F1(a):\n\n\tfunction F1_inner():\n\nfunction F2(b):\n\n'
-    self.bList['variables_only'] = 'A = 1\n\n\tA_ignored = 11\n\nD=4\n\nC\t=\t3\n\nlocal B = None\n\n'
+    self.bList['variables_only'] = 'A = 1\n\n\tA_ignored = 11\n\nD=4\n\nC\t=\t3\n\nlocal B = None\n\nmod.E = true\n\n'
     self.bList['root_items'] = 'function F1()\nclass C1()\nV1 = 1\nclass C2()\nV2 = 2\nlocal function F2()\n'
     self.bList['full'] =
         'VAR1 = 1\nlocal VAR2 = 2\n\n' ..
-        'class A:\n\tdef __init__(self):\n\tdef __str__(self):\n\tdef __repr__(self):\n' ..
-        'class C:\n\tdef __init__(self):\n\tdef do_something(self):\n' ..
+        'class A\n\tfunction A:__init__()\n\tfunction A:__str__()\n\tfunction A:__repr__()\n' ..
+        'class C\n\tfunction C:__init__()\n\tfunction C:do_something()\n' ..
         'local VAR3 = 3\nVAR4 = 4\n\n' ..
-        'class B(A):\n\tdef __init__(self):\n' ..
-        'function F3():\n' ..
+        'class B(A)\n\tfunction B:__init__()\n' ..
+        'function F3()\n' ..
         'local function F1()\n' ..
-        'local function F2()\n\tlocal function F2_inner():\n'
+        'local function F2()\n\tlocal function F2_inner()\n'
+    self.bList['normal'] =
+        'local tree = {}\n' ..
+        'local DBG = true\n' ..
+        'tree.A = 1\n' ..
+        'tree.B = 2\n' ..
+        'function tree.Node:__init__()\n' ..
+        'function tree.Node:do_some()\n' ..
+        'tree.C = 3\n' ..
+        ''
 end
 
 function TestLuaBuffer:test_export_structure()
     local expected = {}
 
-    expected['functions_only'] = {
-        '. Objects',
-        '',
-        'v Functions',
-        '  . F1',
-        '  . F1_inner',
-        '  . F2',
-        '  . F3',
-        '  . F4',
-        '',
-        '. Variables',
-    }
-    expected['variables_only'] = {
-        '. Objects',
+    -- expected['functions_only'] = {
+        -- '. Objects',
+        -- '',
+        -- 'v Functions',
+        -- '  . F1',
+        -- '  . F1_inner',
+        -- '  . F2',
+        -- '  . F3',
+        -- '  . F4',
+        -- '',
+        -- '. Variables',
+    -- }
+    -- expected['variables_only'] = {
+        -- 'v Objects',
+        -- '  v mod',
+        -- '    . E',
+        -- '',
+        -- '. Functions',
+        -- '',
+        -- 'v Variables',
+        -- '  . A',
+        -- '  . B',
+        -- '  . C',
+        -- '  . D',
+    -- }
+    -- expected['root_items'] = {
+        -- '. Objects',
+        -- '',
+        -- 'v Functions',
+        -- '  . F1',
+        -- '  . F2',
+        -- '',
+        -- 'v Variables',
+        -- '  . V1',
+        -- '  . V2',
+    -- }
+    -- expected['full']   = {
+        -- 'v Objects',
+        -- '  v A',
+        -- '    . __init__',
+        -- '    . __repr__',
+        -- '    . __str__',
+        -- '  v B',
+        -- '    . __init__',
+        -- '  v C',
+        -- '    . __init__',
+        -- '    . do_something',
+        -- '',
+        -- 'v Functions',
+        -- '  . F1',
+        -- '  . F2',
+        -- '  . F3',
+        -- '',
+        -- 'v Variables',
+        -- '  . VAR1',
+        -- '  . VAR2',
+        -- '  . VAR3',
+        -- '  . VAR4',
+    -- }
+    expected['normal']  = {
+        'v Objects',
+        '  v tree',
+        '    v Node',
+        '      . __init__',
+        '      . do_some',
+        '    . A',
+        '    . B',
+        '    . C',
         '',
         '. Functions',
         '',
         'v Variables',
-        '  . A',
-        '  . B',
-        '  . C',
-        '  . D',
-    }
-    expected['root_items'] = {
-        '. Objects',
-        '',
-        'v Functions',
-        '  . F1',
-        '  . F2',
-        '',
-        'v Variables',
-        '  . V1',
-        '  . V2',
-    }
-    expected['full']   = {
-        '. Objects',
-        '',
-        'v Functions',
-        '  . F1',
-        '  . F2',
-        '  . F3',
-        '',
-        'v Variables',
-        '  . VAR1',
-        '  . VAR2',
-        '  . VAR3',
-        '  . VAR4',
+        '  . DBG',
     }
 
     for k, v in pairs(expected) do
         local luastr = self.bList[k]
-        local ttree = lgl.tree_to_navbar(lgl.export_structure(luastr))
+        local root = lgl.export_structure(luastr)
+        local ttree = lgl.tree_to_navbar(root)
+
+        -- if k == 'normal' then
+            -- print('\n' .. root:tree() .. '\n')
+--
+            -- for i, t in ipairs(ttree) do
+                -- print(t['text'])
+            -- end
+        -- end
+
         for i, t in ipairs(ttree) do
             lu.assertEquals(t['text'], expected[k][i])
         end
